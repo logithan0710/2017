534d9f8c2fbe7087c652ffb1ac8aa994f0a48d96
logithan0710 <logi0710@hotmail.com>
2017-01-29 11:52:52 +0530

    Practical
    01.29.2017

---
+++ java/01.29.2017/MyFirstJavaProgram.java
@@ -0,0 +1,9 @@
+public class MyFirstJavaProgram{
+	
+	/* this is my first java program
+	this will print hi im logithan
+	*/
+	public static void main(String []args){
+		System.out.println("Hi Im Logithan"); // print hi im logithan
+	}
+}
